0761a7efa1f4608c2268302a3387bff33f62e8ea
Mariana da Costa <marianacosta.engc@gmail.com>
2022-11-25 23:17:33 -0300

    Adicionando funcionalidade de exportar

--- desafio-estoque/controle-de-estoque/src/main/kotlin/ControleDeEstoque.kt
+++ desafio-estoque/controle-de-estoque/src/main/kotlin/ControleDeEstoque.kt
@@ -1,3 +1,4 @@
+import java.io.File
 import javax.naming.LimitExceededException
 import kotlin.system.exitProcess
 
@@ -7,6 +8,7 @@ const val opcoesDoMenu = """
     2 - EDITAR ITEM
     3 - EXIBIR ITENS EM ESTOQUE
     4 - EXIBIR TODOS OS ITENS
+    5 - GRAVAR ITENS NO TXT
     0 - FECHAR SISTEMA
 """
 
@@ -23,11 +25,10 @@ fun menu() {
             1 -> adicionarItem()
             2 -> editarItem()
             3 -> exibirProdutosEmEstoque()
-            4 -> exibirListaDeProdutos()
+            4 -> exibirListaDeProdutos(true)
+            5 -> gravandoLista(true)
             0 -> fecharPrograma()
         }
-    } catch (e: LimitExceededException) {
-        print(e.message)
     } catch (e: Exception) {
         println("Opção incorreta. Digite uma das seguintes opções: $opcoesDoMenu")
         menu()
@@ -35,30 +36,34 @@ fun menu() {
 }
 
 fun adicionarItem() {
-    if (listaDeProdutos.size == 999) {
-        throw LimitExceededException("LimiteEstoqueMaxException")
-    } else {
-        val qtd: Int
-        val index = listaDeProdutos.size.inc()
-        print("Informe o nome do produto: ")
-        try {
-            val produto = readLine().toString()
-            print("Informe a quantidade: ")
-            qtd = readLine()?.toInt() ?: 0
+    val qtd: Int
+    val index = listaDeProdutos.size.inc()
+    print("Informe o nome do produto: ")
+    try {
+        val produto = readLine().toString()
+        print("Informe a quantidade: ")
+        qtd = readLine()?.toInt() ?: 0
+        if (qtd > 999) {
+            throw LimitExceededException("LimiteEstoqueMaxException")
+        } else {
             val item = item(index, produto, qtd)
             listaDeProdutos.add(Triple(index, item, qtd))
             menu()
-        } catch (e: Exception) {
-            println("!!! A quantidade precisa ser um número maior ou igual a zero !!! \n")
-            adicionarItem()
         }
+    } catch (e: LimitExceededException) {
+        println(e.message)
+        println("!!! A quantidade precisa ser um número entre 0 e 999 !!! \n")
+        adicionarItem()
+    } catch (e: Exception) {
+        println("!!! A quantidade precisa ser um número entre 0 e 999 !!! \n")
+        adicionarItem()
     }
 }
 
 fun editarItem() {
     if (listaDeProdutos.isNotEmpty()) {
         println("Qual desses itens deseja editar? ")
-        exibirListaDeProdutos()
+        exibirListaDeProdutos(false)
         print("\n Digite o ID do produto que deseja editar: ")
         try {
             val index = readLine()?.toInt() ?: -1
@@ -67,10 +72,17 @@ fun editarItem() {
             val novoNome = readLine().toString()
             print("Informe a nova quantidade: ")
             val novaQuantidade = readLine()?.toInt() ?: 0
-            val itemEditado = item(listaDeProdutos[index].first, novoNome, novaQuantidade)
-            listaDeProdutos[0] = Triple(index, itemEditado, novaQuantidade)
-            menu()
-            exibirListaDeProdutos()
+            if (novaQuantidade > 999) {
+                throw LimitExceededException("LimiteEstoqueMaxException")
+            } else {
+                val itemEditado = item(listaDeProdutos[index - 1].first, novoNome, novaQuantidade)
+                listaDeProdutos[index - 1] = Triple(index, itemEditado, novaQuantidade)
+                println("Produto alterado com sucesso: ${listaDeProdutos[index - 1].second}")
+            }
+        } catch (e: LimitExceededException) {
+            println(e.message)
+            println("!!! A quantidade precisa ser um número entre 0 e 999 !!! \n")
+            editarItem()
         } catch (e: IndexOutOfBoundsException) {
             println("Você digitou incorretamente")
             editarItem()
@@ -82,36 +94,42 @@ fun editarItem() {
         println("A lista está vazia =(")
         menu()
     }
+    menu()
 }
 
 fun exibirProdutosEmEstoque() {
     if (listaDeProdutos.isNotEmpty()) {
         val produtosEmEstoque = listaDeProdutos.filter { it.third > 0 }
         println("ID | Peça | Quantidade")
-        produtosEmEstoque.forEach {
-            println(it.second)
-        }
+        produtosEmEstoque.forEach { println(it.second) }
     } else {
         println("A lista está vazia =(")
     }
     menu()
 }
 
-fun exibirListaDeProdutos() {
+fun exibirListaDeProdutos(exibirMenu: Boolean) {
     if (listaDeProdutos.isNotEmpty()) {
         println("ID | Peça | Quantidade")
-        listaDeProdutos.forEach {
-            println(it.second)
-        }
+        listaDeProdutos.forEach { println(it.second) }
     } else {
         println("A lista está vazia =(")
     }
-    menu()
+    if (exibirMenu) menu()
 }
 
 fun fecharPrograma() {
-    println("********** Volte sempre! =) **********")
+    println("*************************** Volte sempre! =) ***************************")
+    gravandoLista(false)
     exitProcess(0)
+}
+
+fun gravandoLista(exibirMenu: Boolean) {
+    val arquivo = File("listaDeProdutos.txt")
+    arquivo.writeText("ID  |   Peça   | Quantidade\n\n")
+    listaDeProdutos.forEach { arquivo.appendText("${it.second}\n") }
+    println("********** Sua lista foi salva no arquivo listaDeProdutos.txt **********")
+    if (exibirMenu) menu()
 }
 
 fun item(id: Int, peca: String, qtd: Int): String = "#$id | $peca | $qtd"
